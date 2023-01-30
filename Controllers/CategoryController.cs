605811a90e994eb041e65ff51cf17592f10c2bb7
ZvalsoraY <yazamail@yandex.ru>
2023-01-30 22:03:11 +0300

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
