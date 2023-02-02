93f72b39e61974c28e02f43e0663f6565604effc
ZvalsoraY <yazamail@yandex.ru>
2023-02-02 06:04:10 +0300

    37_007

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -20,11 +20,13 @@ namespace Shop.Controllers
             IEnumerable<Category> objList = _db.Category;
             return View(objList);
         }
+
         // get - create
         public IActionResult Create()
         {
             return View();
         }
+
         // post - create
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -39,6 +41,7 @@ namespace Shop.Controllers
             return View(obj);
             //return View();
         }
+
         // get - edit
         public IActionResult Edit(int? id)
         {
@@ -53,6 +56,51 @@ namespace Shop.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        // post - edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // get - delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // post - delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");;
         }
     }
 }
