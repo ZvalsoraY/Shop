276c13bd2c4ae04ca8fecaaa91a9f63f0d5550ea
ZvalsoraY <yazamail@yandex.ru>
2023-02-08 21:00:19 +0300

    60_001

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,6 +8,7 @@ namespace Shop.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ShortDesc { get; set; }
         public string Description { get; set; }
         [Range(1,int.MaxValue)]
         public double Price { get; set; }
