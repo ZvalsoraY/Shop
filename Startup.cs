7e95afc48d358e0cb8a2aca71e22b6e29b6d3ff1
ZvalsoraY <yazamail@yandex.ru>
2023-02-11 13:05:43 +0300

    76_005

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,8 @@ namespace Shop
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultTokenProviders().AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
